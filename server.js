192340108e98d0eca51cd240d32b4f80293f9d34
kao.ring <kao.ring@yahoo.com>
2020-08-03 03:52:36 -0400

    almost done

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
 
 // Sets up the Express App
 // =============================================================
@@ -25,4 +26,38 @@ app.get("/reserve", function (req, res) {
 });
 app.get("/table", function (req, res) {
   res.sendFile(path.join(__dirname, "public", "table.html"));
+});
+
+//Routes API
+app.get("/api/tables", function (req, res) {
+  return res.json(JSON.parse(fs.readFileSync("db/table.json")));
+});
+app.get("/api/waitlist", function (req, res) {
+  return res.json(JSON.parse(fs.readFileSync("db/waitlist.json")));
+});
+
+//POST
+app.post("/api/tables", function (req, res) {
+  if (tables.length < 5) {
+    tables.push(req.body);
+    fs.writeFileSync("db/table.json", JSON.stringify(tables));
+    res.json(true);
+  } else {
+    waitlist.push(req.body);
+    fs.writeFileSync("db/waitlist.json", JSON.stringify(waitlist));
+    res.json(false);
+  }
+});
+
+//DELETE
+app.post("/api/clear", function (req, res) {
+  tables = [];
+  waitlist = [];
+  fs.writeFileSync("db/table.json", JSON.stringify(tables));
+  fs.writeFileSync("db/waitlist.json", JSON.stringify(waitlist));
+  res.json(true);
+});
+//Listen
+app.listen(PORT, function () {
+  console.log("App listening on http://localhost:" + PORT);
 });
